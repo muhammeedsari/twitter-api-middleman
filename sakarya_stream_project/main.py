8b3790979694af9ee9ec5a47c76d05e7e3690e96
Muhammed Sarı <72307507+muhammeedsari@users.noreply.github.com>
2022-01-03 15:34:15 +0300

    update
    update

--- sakarya_stream_project/main.py
+++ sakarya_stream_project/main.py
@@ -1,15 +1,13 @@
 from controller.produceController.streamListener import StreamListener
 
-consumer_key="VgI4nJ630p2gQWA0GCybdN3tn"
-consumer_secret="6bQbRF69jA4EBxINOw6bWoZZnafYSpcZineRO08tmwO4bJQA5l"
-access_token_key="4857953291-WBOF4xnboxu1AyHW90P85y1JyhHOAghpKm0P7g3"
-access_token_secret="tfBPKie1FQVjfMr1qRsifLADdvoiK1SOOlepXiRuzBmMq"
-
+consumer_key = ""
+consumer_secret = ""
+access_token_key = ""
+access_token_secret = ""
 
 
 stream = StreamListener(
-  consumer_key, consumer_secret,
-  access_token_key, access_token_secret
+    consumer_key, consumer_secret, access_token_key, access_token_secret
 )
 
-stream.filter(track=["ekonomi"],languages=["tr"])
+stream.filter(track=["ekonomi"], languages=["tr"])
